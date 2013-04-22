4e7eff54463b32103034c9b2aa6bc8a1a3406d7c
moz <moz@monlennart.lan>
2013-04-22 21:55:11 +0200

    Initial motions at enabling command line specification of outfile

--- ConGraph/src/ListFiles.py
+++ ConGraph/src/ListFiles.py
@@ -3,11 +3,6 @@ Created on 3 Apr 2013
 
 @author: moz
 
-
-
-
-
-
 '''
 import os.path
 import xml.etree.ElementTree as etree
@@ -16,35 +11,27 @@ import sys
 import xml.etree.ElementTree
 
 BaseDir = "/nobackup/moz/Downloads/sdcard_fix/doc/doxygen/xml"
-
+FilesGraphDotFilename = "FileIncludes.dot"
 
 def ReadCompound( c ):
     RetDict = {}
     RetDict['Label'] = c[0].text
-    RetDict['Id'] = c.attrib['id']
+    RetDict['Id'] = c.attrib['id'].replace('-', '_')
     RetDict['Kind'] = c.attrib['kind']
     RetDict['IncludedCount'] = len(c.findall('includedby'))
     
-#    print "- Readable name: %s" % RetDict['Label']
-#    print "- Kind: %s" % RetDict['Kind']
-#    print "- Id: %s" % RetDict['Id']
-#    print "- Included count: %d" % RetDict['IncludedCount']
-#    print "- Includes"
     IncList = []
     for i in c.findall('includes'):
         try:
             if i.attrib['local'] == 'yes':
-                IncList.append(i.attrib['refid'])
+                IncList.append(i.attrib['refid'].replace('-', '_'))
         except KeyError:
             print >> sys.stderr, "Key error in compound %s includes entry %s - skipping"%(RetDict['Label'], i.text)
-#        else:
-#            print "-- skipping non-local %s" % i.text
-    
-#    print "-- %s" % IncList
+
     RetDict['IncludeList'] = IncList
     return RetDict
 
-def ReadFile(filename, MaxIncCount = -1, MaxIncCountId = "none"):
+def ExtractFileRelation(filename, MaxIncCount = -1, MaxIncCountId = "none"):
     print "// %s" % "Reading compounds in file %s" % filename
     tree = etree.parse(filename)
     root = tree.getroot()
@@ -62,18 +49,18 @@ def ReadFile(filename, MaxIncCount = -1, MaxIncCountId = "none"):
             continue
         
             
-        print "node [shape = circle,width=1,fixedsize=true]; %s;" % ( NodeInfo['Id'])
+        print 'node [shape = circle,width=1]; "%s";' % ( NodeInfo['Id'])
         
         # todo: We want some visual thing to say if we have a high IncludedCount
         #print "node [shape = circle,width=%d,fixedsize=true]; %s;" % (NodeInfo['IncludedCount'] + 1, NodeInfo['Id'])
         for inc in NodeInfo['IncludeList']:
-            print "%s -> %s;" % (NodeInfo['Id'], inc)
+            print '"%s" -> "%s";' % (NodeInfo['Id'], inc)
             
         if NodeInfo['IncludedCount'] > MaxIncCount:
             MaxIncCount = NodeInfo['IncludedCount']
             MaxIncCountId = NodeInfo['Id']
             
-    print "" # output candy
+        print "" # output candy
     return MaxIncCount, MaxIncCountId
 
 DigraphHeader = '''digraph Files {
@@ -85,18 +72,20 @@ DigraphFooter = '''
 '''    
     
 if __name__ == '__main__':
-    print "// %s"%"Parsing xml files from %s"%BaseDir
+    
+    #RelDotFile = open( FilesGraphDotFilename, 'w+' )
+    RelDotFile = sys.stdout
+    
+    print >> RelDotFile, "// %s"%"Parsing xml files from %s"%BaseDir
 
-    print DigraphHeader
+    print >> RelDotFile, DigraphHeader
     MaxIncCount, MaxIncCountId = -1, "none"
     
     for filename in glob.glob(os.path.join(BaseDir, '*.xml')):
-        #Filename = "analog_8c.xml"
-        #FullFilename = os.path.join( BaseDir, Filename )
         try:
-            MaxIncCount, MaxIncCountId = ReadFile(filename, MaxIncCount, MaxIncCountId )
+            MaxIncCount, MaxIncCountId = ExtractFileRelation(filename, MaxIncCount, MaxIncCountId )
         except xml.etree.ElementTree.ParseError:
-            print sys.stderr, "malformed xml in file %s - skipping"%filename
+            print >> sys.stderr, "malformed xml in file %s - skipping"%filename
     
-    print 'graph [ranksep=3, root="%s"];'%MaxIncCountId
-    print DigraphFooter
+    print >> RelDotFile, 'graph [ranksep=3, root="%s"];'%MaxIncCountId
+    print >> RelDotFile, DigraphFooter
